a8f33f60dcf4e59c367003b45d586a12040736df
KAAAsS <admin@kaaass.net>
2020-10-24 00:25:30 +0800

    增加字体反锯齿

--- src/net/kaaass/ospage/Entry.java
+++ src/net/kaaass/ospage/Entry.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 
 public class Entry {
     public static void main(String[] args) {
+        // 字体反锯齿
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
         // 设置Gtk风格GUI
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
